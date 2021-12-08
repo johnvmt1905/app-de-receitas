0d4e5ffe53a399188fff038ea07120006f749c05
Arthur <viegas.arthur2013@hotmail.com>
2021-12-08 16:24:05 -0300

    cria e configura arquivos do context

---
+++ src/context/AppProvider.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import AppContext from './AppContext';
+
+function AppProvider({ children }) {
+  const INITIAL_STATE = {};
+  const [state, setState] = useState(INITIAL_STATE);
+  return (
+    <AppContext.Provider value={ { state, setState } }>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+AppProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AppProvider;
