b500a81735e054f47b8f10e9712cfc891a928c07
JoaoVMont <joaovmt1905@gmail.com>
2021-12-09 15:42:10 -0300

    Tela de Perfil

---
+++ src/pages/Perfil.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+
+function Perfil() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
+  const history = useHistory();
+
+  function recipesDone() {
+    history.push('/receitas-feitas');
+  }
+
+  function recipesFavorites() {
+    history.push('/receitas-favoritas');
+  }
+
+  function LogoutButton() {
+    const user = {
+      email: '',
+    };
+
+    localStorage.setItem('user', JSON.stringify(user));
+    history.push('/');
+  }
+
+  return (
+    <div>
+      <h2 data-testid="profile-email">
+        {email}
+      </h2>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ recipesDone }
+      >
+        Receitas Feitas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ recipesFavorites }
+      >
+        Receitas Favoritas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ LogoutButton }
+      >
+        Sair
+      </button>
+    </div>
+  );
+}
+
+export default Perfil;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import Login from '../pages/Login';
 import NotFound from '../pages/NotFound';
+import Perfil from '../pages/Perfil';
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/perfil" component={ Perfil } />
       <Route exact path="*" component={ NotFound } />
     </Switch>
   );
