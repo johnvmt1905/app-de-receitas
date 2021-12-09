d1d99f0e7061d1f6278e746e23463d1715b9d389
Arthur <viegas.arthur2013@hotmail.com>
2021-12-09 12:50:50 -0300

    Adiciona estado de condicional para ativação da barra de busca

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -5,8 +5,10 @@ import AppContext from './AppContext';
 function AppProvider({ children }) {
   const INITIAL_STATE = {};
   const [state, setState] = useState(INITIAL_STATE);
+  const [searchBar, toggleSearchBar] = useState(false);
+
   return (
-    <AppContext.Provider value={ { state, setState } }>
+    <AppContext.Provider value={ { state, setState, searchBar, toggleSearchBar } }>
       {children}
     </AppContext.Provider>
   );
