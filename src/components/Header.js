4ec30f5f6caa65d2ac09b3fa9c8fb2bb6b053ac4
Arthur <viegas.arthur2013@hotmail.com>
2021-12-09 16:57:31 -0300

    Refatora Header para executar dinamicamente

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,58 @@
-import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 import '../assets/css/header.css';
-import AppContext from '../context/AppContext';
 
-function Header() {
-  const { searchBar, toggleSearchBar } = useContext(AppContext);
+function searchBarIcon(toggleSearchBar, searchBar) {
+  return (
+    <input
+      data-testid="search-top-btn"
+      type="image"
+      alt="SearchIcon"
+      src={ searchIcon }
+      onClick={ () => {
+        toggleSearchBar(!searchBar);
+      } }
+    />
+  );
+}
+
+// expera receber prop "pageTitle"  com o nome da página
+function Header({ pageTitle = 'nome da página' }) {
+  const [searchBar, toggleSearchBar] = useState(false);
+  const hasButton = () => pageTitle === 'Comidas' || pageTitle === 'Explorar Origem';
   const history = useHistory();
   return (
     <header className="header-div">
       <nav className="header-nav">
-        <button
-          type="button"
-          onClick={ () => {
-            // substituir "teste" pela roda de "tela de parfil"
-            history.push('/teste');
-          } }
-        >
-          <img
-            src={ profileIcon }
-            alt="profileIcon"
-          />
-        </button>
+        <input
+          data-testid="profile-top-btn"
+          type="image"
+          alt="Imagem do perfil"
+          src={ profileIcon }
+          onClick={ () => history.push('/teste') }
+        />
         <Link
-          to="/"
+          data-testid="page-title"
+          to="/comidas"
         >
-          Receitas
+          { pageTitle }
         </Link>
-        <button
-          type="button"
-          onClick={ () => {
-            toggleSearchBar(!searchBar);
-          } }
-        >
-          <img src={ searchIcon } alt="searchIcon" />
-        </button>
+        <div>
+          { hasButton() && searchBarIcon(toggleSearchBar, searchBar) }
+        </div>
       </nav>
       {/* subtituir componente de teste pelo componente "barra de busca" */}
       {/* {searchBar && <Teste />} */}
     </header>
   );
 }
+
+Header.propTypes = {
+  pageTitle: PropTypes.string.isRequired,
+};
 
 export default Header;
