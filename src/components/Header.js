990270b474e0b6f19549bd57e9500a96941bbd73
Arthur <viegas.arthur2013@hotmail.com>
2021-12-09 17:30:07 -0300

    Adiciona input de pesquisa

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,7 @@ import '../assets/css/header.css';
 function searchBarIcon(toggleSearchBar, searchBar) {
   return (
     <input
+      className="input-buttons"
       data-testid="search-top-btn"
       type="image"
       alt="SearchIcon"
@@ -23,31 +24,41 @@ function searchBarIcon(toggleSearchBar, searchBar) {
 // expera receber prop "pageTitle"  com o nome da página
 function Header({ pageTitle = 'nome da página' }) {
   const [searchBar, toggleSearchBar] = useState(false);
-  const hasButton = () => pageTitle === 'Comidas' || pageTitle === 'Explorar Origem';
+  const hasButton = () => pageTitle === 'Explorar' || pageTitle === 'Explorar Origem';
   const history = useHistory();
   return (
-    <header className="header-div">
-      <nav className="header-nav">
-        <input
-          data-testid="profile-top-btn"
-          type="image"
-          alt="Imagem do perfil"
-          src={ profileIcon }
-          onClick={ () => history.push('/teste') }
-        />
-        <Link
-          data-testid="page-title"
-          to="/comidas"
-        >
-          { pageTitle }
-        </Link>
-        <div>
-          { hasButton() && searchBarIcon(toggleSearchBar, searchBar) }
+    <>
+      <header className="header-div">
+        <nav className="header-nav">
+          <input
+            className="input-buttons"
+            data-testid="profile-top-btn"
+            type="image"
+            alt="Imagem do perfil"
+            src={ profileIcon }
+            onClick={ () => history.push('/teste') }
+          />
+          <Link
+            data-testid="page-title"
+            to="/comidas"
+          >
+            { pageTitle }
+          </Link>
+          <div>
+            { hasButton() && searchBarIcon(toggleSearchBar, searchBar) }
+          </div>
+        </nav>
+      </header>
+      {searchBar && (
+        <div className="search-input-div">
+          <input
+            className="search-input"
+            type="text"
+            data-testId="search-input"
+          />
         </div>
-      </nav>
-      {/* subtituir componente de teste pelo componente "barra de busca" */}
-      {/* {searchBar && <Teste />} */}
-    </header>
+      )}
+    </>
   );
 }
 
