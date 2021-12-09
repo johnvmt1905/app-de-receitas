1a1a2f54796aa2a1222bb2165fd7f7fdf29f63a3
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2021-12-09 18:10:08 -0300

    Adicionando Bebida e comida na pagina principal

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -1,12 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import AppContext from './AppContext';
 
 function AppProvider({ children }) {
-  const INITIAL_STATE = {};
-  const [state, setState] = useState(INITIAL_STATE);
+  const INITIAL_STATE = {
+    meals: [],
+    drinks: [],
+  };
+
+  const [data, setData] = useState(INITIAL_STATE);
+
+  const [recipeIngredients, setRecipeIngredients] = useState(INITIAL_STATE);
+
+  const [comida, setComida] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+
+  const fetchAPI = async (url) => fetch(url)
+    .then((response) => response.json());
+
+  const getDataFromAPI = async () => {
+    const mealsList = await fetchAPI('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    const drinksList = await fetchAPI('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    setData({
+      meals: mealsList.meals,
+      drinks: drinksList.drinks,
+    });
+  };
+
+  useEffect(() => {
+    getDataFromAPI();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <AppContext.Provider value={ { state, setState } }>
+    <AppContext.Provider
+      value={ { data,
+        setData,
+        recipeIngredients,
+        setRecipeIngredients,
+        comida,
+        setComida,
+        drinks,
+        setDrinks,
+        getDataFromAPI } }
+    >
       {children}
     </AppContext.Provider>
   );
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import MainPage from '../pages/MainPage';
+import Bebidas from '../pages/Bebidas';
+import Comidas from '../pages/Comidas';
 import NotFound from '../pages/NotFound';
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={ MainPage } />
+      <Route exact path="/comidas" component={ Comidas } />
+      <Route path="/bebidas" component={ Bebidas } />
       <Route exact path="*" component={ NotFound } />
     </Switch>
   );
