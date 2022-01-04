3d410fffeecd64b64c9ededaec4709d4e72a9959
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2022-01-04 18:39:48 -0300

    requisitos 67,68,69

--- src/pages/Explorer.js
+++ src/pages/Explorer.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Explorer() {
   return (
-    <>
-      <Header pageTitle="Explorar" />
+    <section>
+      <Header componentName="Explorar" />
+      <h1>Explorar</h1>
+      <section>
+        <Link to="/explorar/comidas">
+          <button data-testid="explore-food" type="button">
+            Explorar Comidas
+          </button>
+        </Link>
+
+        <Link to="/explorar/bebidas">
+          <button data-testid="explore-drinks" type="button">
+            Explorar Bebidas
+          </button>
+        </Link>
+      </section>
       <Footer />
-    </>
+    </section>
   );
 }
 
