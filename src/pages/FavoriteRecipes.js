7f9105a8a45242f32f905d6c0ff3fa3c95a40bab
Arthur <viegas.arthur2013@hotmail.com>
2022-01-05 09:36:44 -0300

    implement favorite recipes page

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import Header from '../components/Header';
+import CardReceita from '../components/CardReceita';
+import '../assets/css/favorite.css';
+
+function getFavorites() {
+  const userFavorites = localStorage.getItem('favoriteRecipes');
+  const data = JSON.parse(userFavorites);
+  return data;
+}
 
 function FavoriteRecipes() {
+  const favorites = getFavorites();
   return (
-    <Header pageTitle="Receitas Favoritas" />
+    <>
+      <Header pageTitle="Receitas Favoritas" />
+      <section className="card-favContainer">
+        { favorites.map((recipe, index) => (
+          <CardReceita
+            pageTitle="Receitas Favoritas"
+            key={ index }
+            recipe={ recipe }
+            index={ index }
+          />
+        )) }
+      </section>
+    </>
   );
 }
 
