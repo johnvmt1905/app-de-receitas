05454f986c24b8f5be837bbd36e0603cb47ad556
Arthur Teixeira <tuzinf5@gmail.com>
2022-01-11 19:59:18 -0300

    Tirando linha sobrando

--- src/components/CardIngredients.js
+++ src/components/CardIngredients.js
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import '../assets/css/recipeCard.css';
 
-
 function CardIngredients(props) {
   const { name, img, index } = props;
 
